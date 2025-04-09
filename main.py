4cec52f3bdaf77bab140b9f97ced4602d8a495c4
xinliang <leptobo@163.com>
2025-04-09 20:57:32 +0800

    整理项目结构

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 """
-# 构建docker镜像
+
+
+# 构建docker镜像,构建过程会下载依赖
 cd corgi
 docker build -t corgi:latest .
 
-# 运行docker容器
+# 运行docker容器,映射当前目录下的 screenshots 和 logs 目录到容器内
+# 测试环境
 docker run \
+    -d \
+    --rm \
     -v "$(pwd)/screenshots:/corgi/screenshots" \
     -v "$(pwd)/logs:/corgi/logs" \
     --name corgi \
     corgi:latest
-运行过程的截图会放到 $(pwd)/screenshots/ 目录下
 
 
+# 调试环境
+docker run \
+    -it \
+    --rm \
+    -v "$(pwd)/screenshots:/corgi/screenshots" \
+    -v "$(pwd)/logs:/corgi/logs" \
+    --name corgi \
+    corgi:latest \
+    /bin/bash
+
+    
 """
-
-print("hello world")
