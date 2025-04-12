6d473357aea61ce78f0bfaf42a442a28569f69c8
xinliang <leptobo@163.com>
2025-04-12 19:25:08 +0800

    add overlapped page

--- pages/overlapped_page.py
+++ pages/overlapped_page.py
@@ -3,7 +3,27 @@ from pages.base_page import BasePage
 
 class OverlappedPage:
     def __init__(self, page: BasePage):
-        super().__init__(page)
+        self.page = page
+        self.url = "https://www.uitestingplayground.com/overlapped"
 
-    def fill_input(self, text):
-        self.page.fill("#overlappedInput", text)
+        self.name_input_selector = "#name"
+        self.id_input_selector = "#id"
+        self.subject_input_selector = "#subject"
+
+    def input_id(self, text: str):
+        self.page.execute_script(
+            f"arguments[{self.page.find(self.id_input_selector)}].scrollIntoView(true);"
+        )
+        self.page.find(self.id_input_selector).input(text)
+
+    def input_name(self, text: str):
+        self.page.execute_script(
+            f"arguments[{self.page.find(self.name_input_selector)}].scrollIntoView(true);"
+        )
+        self.page.find(self.name_input_selector).input(text)
+
+    def input_subject(self, text: str):
+        self.page.execute_script(
+            f"arguments[{self.page.find(self.subject_input_selector)}].scrollIntoView(true);"
+        )
+        self.page.find(self.subject_input_selector).input(text)
