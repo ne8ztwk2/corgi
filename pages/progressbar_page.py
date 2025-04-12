699bac3833be6bb93ec144690f3c4b95fa9b01a4
xinliang <leptobo@163.com>
2025-04-12 19:34:13 +0800

    add progressbar page

--- pages/progressbar_page.py
+++ pages/progressbar_page.py
@@ -3,10 +3,20 @@ from pages.base_page import BasePage
 
 class ProgressbarPage:
     def __init__(self, page: BasePage):
-        super().__init__(page)
+        self.page = page
+        self.url = "https://www.uitestingplayground.com/progressbar"
+        self.start_button_selector = "#startButton"
+        self.stop_button_selector = "#stopButton"
+        self.progress_bar_selector = "#progressBar"
+        self.result_attribute = "aria-valuenow"
 
     def start_progress(self):
         self.page.click("#startButton")
 
     def stop_progress(self):
         self.page.click("#stopButton")
+
+    def get_result(self) -> str:
+        return self.page.find(self.progress_bar_selector).get_attribute(
+            self.result_attribute
+        )
