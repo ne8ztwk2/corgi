fc0c5b93baed94c4559bb7731976d92da25612c5
xinliang <leptobo@163.com>
2025-04-13 09:17:58 +0800

    add.

--- pages/textinput_page.py
+++ pages/textinput_page.py
@@ -3,10 +3,16 @@ from pages.base_page import BasePage
 
 class TextinputPage:
     def __init__(self, page: BasePage):
-        super().__init__(page)
+        self.page = page
+        self.url = "https://www.uitestingplayground.com/textinput"
+        self.input_selector = "#newButtonName"
+        self.button_selector = "#updatingButton"
 
-    def enter_text(self, text):
-        self.page.fill("#newButtonName", text)
+    def input_text(self, text):
+        self.page.find(self.input_selector).input(text)
 
     def click_update_button(self):
-        self.page.click("#updatingButton")
+        self.page.find(self.button_selector).click()
+
+    def get_butten_text(self) -> str:
+        return self.page.find(self.button_selector).get_attribute("value")
