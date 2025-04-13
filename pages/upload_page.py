f31e425a5e1e0f1622f92b7b49b7f562567db065
xinliang <leptobo@163.com>
2025-04-13 09:17:32 +0800

    can not impl.

--- pages/upload_page.py
+++ pages/upload_page.py
@@ -3,7 +3,13 @@ from pages.base_page import BasePage
 
 class UploadPage:
     def __init__(self, page: BasePage):
-        super().__init__(page)
+        self.url = "https://www.uitestingplayground.com/upload"
+        self.page = page
+        self.file_upload_box_selector = ".document-uploader.upload-box"
+        self.frame_selector = 'iframe[src="/static/upload.html"]'
+        self.browse_btn_selector = ".browse-btn"
 
-    def upload_file(self, file_path):
-        self.page.set_input_files("#fileInput", file_path)
+    def upload_file_for_grag(self, filepath): ...
+    def upload_file_for_open_browse_files(self, filepath): ...
+
+    #  can not impl
