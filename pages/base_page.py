9564b4fec83ba15d305db9498e25d1ca34f69e78
xinliang <leptobo@163.com>
2025-04-14 10:40:14 +0800

    添加注释

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,160 +4,149 @@ from playwright.sync_api import Locator
 from config import UI_MAX_WAIT_TIME, TIME_FORMAT, UI_SCREENSHOT_ABS_DIR
 
 
-# class BasePage(ABC):
-#     max_wait_time = UI_MAX_WAIT_TIME
-#     time_format = TIME_FORMAT
-#     screenshot_dir = UI_SCREENSHOT_ABS_DIR
-
-#     @abstractmethod
-#     def goto(self, url: str): ...
-
-#     @abstractmethod
-#     def click(self, selector: str): ...
-
-#     @abstractmethod
-#     def input(self, selector: str, text): ...
-
-#     @abstractmethod
-#     def get_text(self, selector: str) -> str | None: ...
-
-#     @abstractmethod
-#     def get_text_content(self, selector: str) -> str | None: ...
-
-#     @abstractmethod
-#     def is_visible(self, selector: str) -> bool: ...
-
-#     @abstractmethod
-#     def screenshot(self, filename: str): ...
-
-#     @abstractmethod
-#     def get_attribute(self, selector: str, attribute: str) -> str | None: ...
-
-#     @abstractmethod
-#     def get_title(self) -> str: ...
-
-#     @abstractmethod
-#     def get_url(self) -> str: ...
-
-#     @abstractmethod
-#     def close(self): ...
-
-#     @abstractmethod
-#     def quit(self): ...
-
-#     @abstractmethod
-#     def alert(self, action: Literal["accept", "dismiss"], text: str = None): ...
-
-#     @abstractmethod
-#     def get_alert_text(self) -> str: ...
-
-#     @abstractmethod
-#     def execute_script(self, script: str) -> Any: ...
-
-#     @abstractmethod
-#     def wait_visible(
-#         self, selector: str, max_wait_time: float = None
-#     ) -> BaseElement: ...
-
-#     @abstractmethod
-#     def wait_enable(
-#         self, selector: str, max_wait_time: float = None
-#     ) -> BaseElement: ...
-
-#     @abstractmethod
-#     def wait_editable(
-#         self, selector: str, max_wait_time: float = None
-#     ) -> BaseElement: ...
-
-#     @abstractmethod
-#     def wait_clickable(
-#         self, selector: str, max_wait_time: float = None
-#     ) -> BaseElement: ...
-
-#     @abstractmethod
-#     def wait_non_zreo_size(
-#         self, selector: str, max_wait_time: float = None
-#     ) -> BaseElement: ...
-
-#     @abstractmethod
-#     def select_by_value(self, selector: str, value: str): ...
-
-#     @abstractmethod
-#     def select_by_index(self, selector: str, index: int): ...
-
-#     @abstractmethod
-#     def select_by_text(self, selector: str, text: str): ...
-
-#     @abstractmethod
-#     def find(self, selector: str) -> BaseElement: ...
-
-#     @abstractmethod
-#     def find_all(self, selector: str) -> list[WebElement] | list[Locator]: ...
-
-
 class BaseElement(ABC):
     max_wait_time = UI_MAX_WAIT_TIME
 
     @abstractmethod
-    def click(self): ...
+    def click(self):
+        """
+        左键点击
+        """
+        pass
 
     @abstractmethod
-    def input(self, text: str): ...
+    def input(self, text: str):
+        """
+        输入文字
+        """
+        pass
 
     @abstractmethod
-    def get_text(self) -> str | None: ...
+    def get_text(self) -> str | None:
+        """
+        获取元素简单文本
+        """
+        pass
 
     @abstractmethod
-    def get_text_content(self) -> str | None: ...
+    def get_text_content(self) -> str | None:
+        """
+        获取元素原始文本
+        """
+        pass
 
     @abstractmethod
-    def is_visible(self) -> bool: ...
+    def is_visible(self) -> bool:
+        """
+        判断元素是否可见
+        """
+        pass
 
     @abstractmethod
-    def get_attribute(self, attribute: str) -> str | None: ...
+    def get_attribute(self, attribute: str) -> str | None:
+        """
+        获取元素属性
+        """
+        pass
 
     @abstractmethod
-    def wait_visible(self, max_wait_time: float = None) -> Self: ...
+    def wait_visible(self, max_wait_time: float = None) -> Self:
+        """
+        等待元素可见
+        """
+        pass
 
     @abstractmethod
-    def wait_enable(self, max_wait_time: float = None) -> Self: ...
+    def wait_enable(self, max_wait_time: float = None) -> Self:
+        """
+        等待元素启用
+        """
+        pass
 
     @abstractmethod
-    def wait_editable(self, max_wait_time: float = None) -> Self: ...
+    def wait_editable(self, max_wait_time: float = None) -> Self:
+        """
+        等待元素可编辑
+        """
+        pass
 
     @abstractmethod
-    def wait_clickable(self, max_wait_time: float = None) -> Self: ...
+    def wait_clickable(self, max_wait_time: float = None) -> Self:
+        """
+        等待元素可点击
+        """
+        pass
 
     @abstractmethod
-    def wait_non_zreo_size(self, max_wait_time: float = None) -> Self: ...
+    def wait_non_zreo_size(self, max_wait_time: float = None) -> Self:
+        """
+        等待元素非零尺寸
+        """
+        pass
 
     @abstractmethod
-    def select_by_value(self, value: str): ...
+    def select_by_value(self, value: str):
+        """
+        通过值选择下拉框选项
+        """
+        pass
 
     @abstractmethod
-    def select_by_index(self, index: int): ...
+    def select_by_index(self, index: int):
+        """
+        通过索引选择下拉框选项
+        """
+        pass
 
     @abstractmethod
-    def select_by_text(self, text: str): ...
+    def select_by_text(self, text: str):
+        """
+        通过文本选择下拉框选项
+        """
+        pass
 
     # actions
     @abstractmethod
-    def move(self): ...
+    def move(self):
+        """
+        移动鼠标至指定位置
+        """
+        pass
 
     @abstractmethod
-    def drag_to(self, target: Self): ...
+    def drag_to(self, target: Self):
+        """
+        左键拖动目标至指定位置
+        """
+        pass
 
     @abstractmethod
-    def double_click(self): ...
+    def double_click(self):
+        """
+        左键双击
+        """
+        pass
 
     @abstractmethod
-    def right_click(self): ...
+    def right_click(self):
+        """
+        右键点击
+        """
+        pass
 
     @abstractmethod
-    def click_hold(self): ...
+    def click_hold(self):
+        """
+        点击并按住左键
+        """
+        pass
 
     @abstractmethod
-    def scroll_into_view(self) -> Self: ...
+    def scroll_into_view(self) -> Self:
+        """
+        垂直方向滚动到元素可见
+        """
+        pass
 
 
 class BasePage(ABC):
@@ -165,47 +154,106 @@ class BasePage(ABC):
     screenshot_dir = UI_SCREENSHOT_ABS_DIR
 
     @abstractmethod
-    def goto(self, url: str) -> Self: ...
+    def goto(self, url: str) -> Self:
+        """
+        访问网址
+        """
+        pass
 
     @abstractmethod
-    def screenshot(self, filename: str): ...
+    def screenshot(self, filename: str):
+        """
+        截图
+        """
+        pass
 
     @abstractmethod
-    def get_title(self) -> str: ...
+    def get_title(self) -> str:
+        """获取当前窗口标题"""
+        pass
 
     @abstractmethod
-    def get_url(self) -> str: ...
+    def get_url(self) -> str:
+        """
+        获取当前窗口URL
+        """
+        pass
 
     @abstractmethod
-    def close(self): ...
+    def close(self):
+        """
+        关闭当前窗口
+        """
+        pass
 
     @abstractmethod
-    def quit(self): ...
+    def quit(self):
+        """
+        关闭浏览器
+        """
+        pass
 
     @abstractmethod
-    def alert(self, action: Literal["accept", "dismiss"], text: str = None): ...
+    def alert(self, action: Literal["accept", "dismiss"], text: str = None):
+        """
+        操作alert弹窗
+        """
+        pass
 
     @abstractmethod
-    def get_alert_text(self) -> str: ...
+    def get_alert_text(self) -> str:
+        """
+        获取alert弹窗文字
+        """
+        pass
 
     @abstractmethod
-    def execute_script(self, script: Any, *args: Any) -> Any: ...
+    def execute_script(self, script: Any, *args: Any) -> Any:
+        """
+        执行js脚本
+        """
+        pass
 
     @abstractmethod
-    def find(self, selector: str) -> BaseElement: ...
+    def find(self, selector: str) -> BaseElement:
+        """
+        查找元素
+        """
+        pass
 
     @abstractmethod
-    def find_all(self, selector: str) -> list[BaseElement]: ...
+    def find_all(self, selector: str) -> list[BaseElement]:
+        """
+        查找元素列表
+        """
+        pass
 
     @abstractmethod
-    def actions(self, selector: str) -> BaseElement: ...
+    def actions(self, selector: str) -> BaseElement:
+        """
+        动作链
+        return BaseElement
+        """
+        pass
 
     @staticmethod
     @abstractmethod
-    def get_clipboard_content() -> str: ...
+    def get_clipboard_content() -> str:
+        """
+        获取剪贴板内容
+        """
+        pass
 
     @abstractmethod
-    def switch_to_frame(self, element: BaseElement) -> Self: ...
+    def switch_to_frame(self, element: BaseElement) -> Self:
+        """
+        切换frame
+        """
+        pass
 
     @abstractmethod
-    def switch_to_window(self, name: str) -> Self: ...
+    def switch_to_window(self, name: str) -> Self:
+        """
+        切换浏览器窗口
+        """
+        pass
